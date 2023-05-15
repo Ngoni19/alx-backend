76b1c3bc847ccb9d1e841152b280940035b7aecc
Ngoni19 <ngoni19@live.com>
2023-05-15 21:28:39 +0000

    auto Pep

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Script -> index_range function"""
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index & end index."""
     start = (page - 1) * page_size
