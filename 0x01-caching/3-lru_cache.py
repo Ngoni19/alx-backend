6921ddf432aadf551e4e022034657ae073f7d42f
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-23 00:36:22 +0200

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -7,7 +7,7 @@ from base_caching import BaseCaching
 class LRUCache(BaseCaching):
     """ LRUCache """
 
-    definit__(self):
+    init__(self):
         """ Initialize LRUCache """
         super().__init__()
         self.history = []
