5f5cde84ca6850c476a075dea1c38841c2534309
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-23 00:26:08 +0200

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
-""" LIFO caching
+""" Script -> LIFO caching
 """
-from base_caching import BaseCaching
+
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFOCache """
-
-    def ____(self):
-        """ Initialize LIFOCache """
+    """ LIFOCache cls
+    """
+    def __init__(self):
         super().__init__()
         self.history = []
 
     def put(self, key, item):
-        """ Assigns the new item to the dictionary """
-        if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+        """ it assigns the new item to the dict
+        """
+        if not (key is None or item is None):
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
+                 and key not in self.cache_data:
                 print(f'DISCARD: {self.history[-1]}')
                 self.cache_data.pop(self.history[-1])
                 del self.history[-1]
@@ -25,5 +28,8 @@ class LIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Returns the value in self.cache_data linked to key """
-        return self.cache_data.get(key, None)
+        """ Returns: value in self.cache_data linked to key
+        """
+        if key is None or not (key in self.cache_data):
+            return None
+        return self.cache_data[key]
