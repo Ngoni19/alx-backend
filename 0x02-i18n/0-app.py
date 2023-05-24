1aa85988e4185b4ea38581b17520177f5093651a
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-24 03:37:46 +0200

    Create 0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Script -> Basic Flask app module"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def get_index() -> str:
+    """method - index page"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
