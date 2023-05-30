cf07812a116970e6276e0542ac8ec23ed26a1443
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-30 22:23:31 +0200

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
-""" Script-> LRU caching
+""" Script -> LRU caching
 """
-from base_caching import BaseCaching
+
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ LRUCache """
-
+    """ LRUCache class
+    """
     def __init__(self):
-        """ Initialize LRUCache """
         super().__init__()
         self.history = []
 
     def put(self, key, item):
-        """ it assigns the new item to the dict
+        """method: assigns the new item to the dict
         """
         if not (key is None or item is None):
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS\
                  and key not in self.cache_data:
-                print(f'DISCARD: {self.history[-1]}')
-                self.cache_data.pop(self.history[-1])
-                del self.history[-1]
+                print(f'DISCARD: {self.history[0]}')
+                self.cache_data.pop(self.history[0])
+                del self.history[0]
             if key in self.history:
                 del self.history[self.history.index(key)]
             self.history.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Returns the value in self.cache_data linked to key """
-        if key is not None and key in self.cache_data:
-            self.history.remove(key)
+        """ method: returns the value in self.cache_data linked to key
+        """
+        if key is None or not (key in self.cache_data):
+            return None
+        else:
+            del self.history[self.history.index(key)]
             self.history.append(key)
             return self.cache_data[key]
-        return None
