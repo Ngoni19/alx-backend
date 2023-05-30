16cd70f019adc6947ca688f4adb869d89099c6fb
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-05-30 22:14:17 +0200

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -13,15 +13,6 @@ class LRUCache(BaseCaching):
         self.history = []
 
     def put(self, key, item):
-<<<<<<< HEAD
-        """ Assigns the new item to the dictionary """
-        if key is not None and item is not None:
-            if len(
-                    self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print(f'DISCARD: {self.history[0]}')
-                del self.cache_data[self.history[0]]
-                del self.history[0]
-=======
         """ it assigns the new item to the dict
         """
         if not (key is None or item is None):
@@ -30,7 +21,6 @@ class LRUCache(BaseCaching):
                 print(f'DISCARD: {self.history[-1]}')
                 self.cache_data.pop(self.history[-1])
                 del self.history[-1]
->>>>>>> f460d164f8002c7b7c226165bea59d5e8401bed3
             if key in self.history:
                 del self.history[self.history.index(key)]
             self.history.append(key)
